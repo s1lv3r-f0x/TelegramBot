f369b86294fdf602897100fc7be668eecfc32849
Maxim Belov <darthvader4512000@gmail.com>
2021-04-28 22:33:19 +0400

    add db but not working

--- Program.cs
+++ Program.cs
@@ -13,15 +13,43 @@ namespace Telegram_Bot
             telegramBot = new TelegramBotClient(token);
             telegramBot.StartReceiving();
             telegramBot.OnMessage += TelegramBot_OnMessage;
+            Console.ReadKey();
         }
 
         private static async void TelegramBot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             var msg = e.Message;
-            if (msg != null)
+            if (msg.Text == "/subscribe")
             {
-                Console.WriteLine($@"Пришло сообщение {msg.Text}");
-                await telegramBot.SendTextMessageAsync(msg.Chat.Id, msg.Text); 
+                using (var db = new Subscribers())
+                {
+                    if (db.SubscriberID is null)
+                    {
+                        db.SubscriberID.Add(msg.Chat.Id);
+                        db.SubscriberName.Add(msg.Chat.Username);
+                        db.Subscribe.Add(true);
+                        await telegramBot.SendTextMessageAsync(msg.Chat.Id, "Вы успепшно подписались на рассылку!");
+                        db.SaveChanges();
+                    }
+                    if (db.SubscriberID.Contains(msg.Chat.Id))
+                        await telegramBot.SendTextMessageAsync(msg.Chat.Id, "Вы уже подписаны!");
+                    else
+                    {
+                        await telegramBot.SendTextMessageAsync(msg.Chat.Id, "Фигня какая-то!");
+                    }
+                }
+                
+            }
+
+            if (msg.Text == "/dbname")
+            {
+                using (var db = new Subscribers())
+                {
+                    foreach (var name in db.SubscriberName.ToArray())
+                    {
+                        await telegramBot.SendTextMessageAsync(msg.Chat.Id, name);
+                    }
+                }
             }
         }
     }
---
+++ Subscribers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Telegram_Bot
+{
+    public class Subscribers : DbContext
+    {
+        public List<long> SubscriberID { get; set; }
+        public List<string> SubscriberName { get; set; }
+        public List<bool> Subscribe { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=C:\Users\yatin\source\repos\TelegramBot\Subscribers.db");
+    }
+}
